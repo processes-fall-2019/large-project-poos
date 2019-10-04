180ba0250c52c1cfb16212996956ca7920c3a11d
Francisco Franco <fhfranco32@gmail.com>
2019-10-03 23:16:03 -0400

    added morgan dependency for server logs

--- Desktop/fall2019/processess_OOS/large-project/large-project-poos/server.js
+++ Desktop/fall2019/processess_OOS/large-project/large-project-poos/server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const bodyParser = require('body-parser')
 // const cors = require('cors')
+const morgan = require('morgan')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 
 //here we are configuring dist to serve app files
