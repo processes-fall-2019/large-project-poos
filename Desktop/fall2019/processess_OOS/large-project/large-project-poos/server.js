c0e84aef6bc623da89316c750d51e6ba968b9087
Francisco Franco <fhfranco32@gmail.com>
2019-10-03 23:01:44 -0400

    added get request for testing

--- Desktop/fall2019/processess_OOS/large-project/large-project-poos/server.js
+++ Desktop/fall2019/processess_OOS/large-project/large-project-poos/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const serveStatic = require('serve-static')
+const bodyParser = require('body-parser')
+// const cors = require('cors')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 
+app.use(bodyParser.json())
+
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get('/register', (req, res) => {
+  res.send({
+    message: 'Hello!'
+  })
+})
 
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
