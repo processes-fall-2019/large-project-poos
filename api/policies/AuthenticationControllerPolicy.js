12b9f777b786b76d7b709173632bec054c1fd64d
Francisco Franco <fhfranco32@gmail.com>
2019-10-20 19:59:32 -0400

    created a policy using Joi library to detect when an invalid email, username, or password get passed in

---
+++ api/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,39 @@
+const Joi = require('joi')
+
+module.exports = {
+  async register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string(),
+      username: Joi.string()
+    }
+
+    const {error, value} = Joi.validate(req.body, schema)
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a valid email.'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: 'Invalid password.'
+          })
+          break
+        case 'username':
+          res.status(400).send({
+            error: 'Invalid username.'
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid'
+          })
+      }
+    } else {
+      await next()
+    }
+  }
+}
