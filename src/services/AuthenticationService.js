4c6180eb6dd27c55a17991a3c4e89c9099995c67
Francisco Franco <fhfranco32@gmail.com>
2019-10-20 20:02:56 -0400

    added login endpoint to services

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -4,9 +4,9 @@ export default {
   register (credentials) {
     return Api().post('register', credentials)
   },
-  // login (credentials) {
-  //   return Api().post('login', credentials)
-  // },
+  login (credentials) {
+    return Api().post('login', credentials)
+  },
   // contacts (credentials) {
   //   return Api().get('contacts', credentials)
   // },
