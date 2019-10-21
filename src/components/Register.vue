8b538b318842188823b50842094c258440ca6d47
Francisco Franco <fhfranco32@gmail.com>
2019-10-20 20:01:31 -0400

    created register component and endpoint

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,33 +1,71 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input type="" name="username" v-model="username" placeholder="user name"/>&nbsp;&nbsp;
+    <br>
+    <br>
     <input type="email" name="email" v-model="email" placeholder="email"/>&nbsp;&nbsp;
     <input type="password" name="password" v-model="password" placeholder="password"/>
-    <br><br>
-    <button> Register </button>
+    <br>
+    <br>
+      <div class="error" v-html="error"/>
+    <br>
+    <button @click="register"> Register </button>&nbsp;&nbsp;
+    <router-link :to="{name: 'HelloWorld'}">
+      <button> Cancel </button>
+    </router-link>
   </div>
 </template>
 
 <script>
+import AuthenticationService from '../services/AuthenticationService'
+
 export default {
   props: {
   },
   data () {
     return {
+      username: '',
       email: '',
       password: '',
-      firstName: '',
-      lastName: '',
-      userName: '',
+      error: null
     }
   },
   methods: {
     async register () {
+      try {
+        const user = await AuthenticationService.register({
+          username: this.username,
+          email: this.email,
+          password: this.password,
+        })
+
+        console.log('user', user.data)
+
+        if (user.data.error) {
+          alert(user.data.error)  // TODO: CHANGE do not use alert dialogs like Lienecker said, also add one that says "This username is alreayd in use."
+          return false
+        }
+
+        this.$router.push({
+            name: 'HelloWorld'
+        })
+
+        alert('Hello ' + this.username + ' your user was successfully registered!')
+      } catch (error) {
+        console.log('Error registering.')
+        this.error = error.response.data.error
+      }
     }
+  },
+  mounted () {
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .error {
+    color: red;
+  }
 </style>
