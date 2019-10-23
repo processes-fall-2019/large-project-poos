aeccdd2efc866f81491b7868ccbe7ae9d024334a
Francisco Franco <fhfranco32@gmail.com>
2019-10-23 00:29:10 -0400

    created landing page after logging in

---
+++ src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1> Welcome </h1>
+    <UploadFile/>
+  </div>
+</template>
+
+<script>
+import UploadFile from './UploadFile'
+export default {
+  components: {
+    UploadFile,
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .error {
+    color: red;
+  }
+  .center {
+    margin:  auto;
+    align-items: center;
+    justify-content: center;
+    vertical-align: middle;
+  }
+</style>
