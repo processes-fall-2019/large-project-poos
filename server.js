daee30584c97576ffe1af62e78134e2d772519e6
Francisco Franco <fhfranco32@gmail.com>
2019-10-28 17:05:14 -0400

    moved knex database connection to its own file

--- server.js
+++ server.js
@@ -7,31 +7,7 @@ const multer = require('multer')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
-
-// // establish connection with database using Knex ORM
-// var knex = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host: '127.0.0.1',
-//     port: '5437',
-//     user: 'postgres',
-//     password: '',
-//     database: 'large-project-database'
-//   }
-// })
-
-// for production database: establish connection with database using Knex ORM
-var knex = require('knex')({
-  client: 'pg',
-  connection: 'postgres://lwwuqfolbrvbch:40f3de750b5e7d629a90fecc514fe2b67c4dc2a4b6f3662cf83232d7773fd81f@ec2-54-83-52-191.compute-1.amazonaws.com:5432/d3i23g57firu3e',
-  migrations: {
-    tableName: 'knex_migrations'
-  },
-  ssl: true
-})
-
-
-
+const knex = require('./db/db')
 
 
 // const fileFilter = function(req, file, cb) {
