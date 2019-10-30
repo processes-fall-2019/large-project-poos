5640cd6c1b404979a6bfbe63d68117ca6aa6e542
Francisco Franco <fhfranco32@gmail.com>
2019-10-29 20:08:30 -0400

    upload file

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -107,6 +107,7 @@ export default {
         await axios.post('/upload', formData)
         this.message = "Files have been uploaded."
         this.files = []
+        this.$forceUpdate()
       } catch(err) {
         this.message = err.response.data.error
         this.error = true
