8360274f33d86a279c9ef7052cf87ea3b0b6363f
Francisco Franco <fhfranco32@gmail.com>
2019-12-03 23:53:33 -0500

    syntax

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -21,7 +21,7 @@
               <template v-slot:button-content>
                 <em>User</em>
               </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Verify</b-dropdown-item>
               <b-dropdown-item href="#/">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
           </b-navbar-nav>
