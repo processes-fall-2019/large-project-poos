5a4468d1a5adca3c3a2971e4ab57d0d07d8f6d99
Francisco Franco <fhfranco32@gmail.com>
2019-12-03 23:12:22 -0500

    nav chnages

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -17,17 +17,8 @@
             <b-nav-form>
               <!-- <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
               <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button> -->
-              <b-nav-item href="#">Register</b-nav-item>
+              <b-nav-item href="#/register">Register</b-nav-item>
             </b-nav-form>
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template v-slot:button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
