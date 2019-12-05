3608c2c5d088b405b8ce20443162954de8eec767
Francisco Franco <fhfranco32@gmail.com>
2019-12-05 00:58:22 -0500

    syntax

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,20 +7,14 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <!-- <b-nav-item href="#">Register</b-nav-item> -->
-            <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
-          </b-navbar-nav>
-
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-
-
             <b-nav-item-dropdown right>
               <!-- Using 'button-content' slot -->
               <template v-slot:button-content>
                 <em>User</em>
               </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
               <b-dropdown-item href="#">Verify</b-dropdown-item>
               <b-dropdown-item href="#/">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
